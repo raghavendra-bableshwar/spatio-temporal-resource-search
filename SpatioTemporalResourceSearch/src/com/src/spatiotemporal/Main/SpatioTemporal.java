d5c5767749ff2ac6897a67bbf24b594a4342e8ce
c0deForLife <akolkarswapnil89@gmail.com>
2016-05-01 17:39:34 -0500

    Changes to add the automated script and properties file changes for congestion level

--- SpatioTemporalResourceSearch/src/com/src/spatiotemporal/Main/SpatioTemporal.java
+++ SpatioTemporalResourceSearch/src/com/src/spatiotemporal/Main/SpatioTemporal.java
@@ -1,6 +1,10 @@
 package com.src.spatiotemporal.Main;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -8,11 +12,13 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.TreeMap;
 
-import com.src.spatiotemporal.Algorithms.Algorithms;
 import com.src.spatiotemporal.Algorithms.Baseline;
+import com.src.spatiotemporal.Algorithms.Deterministic;
 import com.src.spatiotemporal.Algorithms.Probabilistic;
+//import com.src.spatiotemporal.Algorithms.Probabilistic;
 import com.src.spatiotemporal.Entity.Adjacency;
 import com.src.spatiotemporal.Entity.Block;
 import com.src.spatiotemporal.Entity.Graph;
@@ -38,23 +44,86 @@ public class SpatioTemporal {
 	public static Graph g = new Graph();
 	public static ArrayList<Adjacency> adjacencyList = new ArrayList<Adjacency>();
 	
-	public static final int CONGESTION_LEVEL = 0;
+	public static int CONGESTION_LEVEL = 0;
 
 	public static void populateData(String csvPath) throws IOException, ParseException
 	{
+		/* Load congestion value from properties file */
+		Properties prop = new Properties();
+		InputStream ip = new FileInputStream(csvPath + "\\..\\Congestion.properties");
+		prop.load(ip);
+		String congestionValue = prop.getProperty("CONGESTION_LEVEL");
+		CONGESTION_LEVEL = Integer.parseInt(congestionValue);
+		
+		
+		/* Loading all the application level data in various data structures */
 		Utils.populateNode(csvPath);
 		Utils.populateBlock(csvPath);
 		Utils.populateProjection(csvPath);
 		Utils.populateAdjacency(csvPath);
-		//System.out.println(projectionList.size());
-	//	System.out.println("Adjacency \n:"+g.getCustomBlockDetails().toString()+"\n");
-		//System.out.println("Adjacency \n:"+g.getAdjList());
 		
-		//Should be handled in a switch case in order to 
-		//switch between algos
-		Algorithms chooseAlgo = new Baseline();
-		//Dummy latitude and longitude passed
-		//chooseAlgo.fetchRoute(37.8054164153, -122.4236028968);
-		System.out.println("Done!");
+		Utils.generatePreditiveModel_1(projectionList);
+		
+	}
+	
+	/*The following method is to run the simulations*/
+	
+	/*Simulations.csv contains list of blocks and corresponding timestamp. The following method contain automated script to execute all the 3 algorithms 
+	 * and output on console. You can redirect the standard output to local CSV file. */
+	
+	public static void main(String argus[]) throws IOException, ParseException
+	{
+	
+		/*Set local path of "data files" folder */
+		String localDataFilePath = "C:\\Users\\Swapnil\\git\\spatiotemporalresourcesearch_web\\SpatioTemporalResourceSearch\\WebContent\\data_files";
+		
+		Properties prop = new Properties();
+		InputStream ip = new FileInputStream(localDataFilePath + "\\..\\Congestion.properties");
+		prop.load(ip);
+		String congestionValue = prop.getProperty("CONGESTION_LEVEL");
+		CONGESTION_LEVEL = Integer.parseInt(congestionValue);
+		
+		Utils.populateNode(localDataFilePath);
+		Utils.populateBlock(localDataFilePath);
+		Utils.populateProjection(localDataFilePath);
+		Utils.populateAdjacency(localDataFilePath);
+		
+		Utils.generatePreditiveModel_1(projectionList);
+		
+		BufferedReader br = null;
+		String line = "";
+		br = new BufferedReader(new FileReader(localDataFilePath + "\\Simulations.csv"));
+		line = br.readLine();
+		String cvsSplitBy = ",";		
+
+		System.out.println("Block,Timestamp,Travel time");
+		
+		while((line = br.readLine()) != null)
+		{
+			SimpleDateFormat formatter = new SimpleDateFormat("y-M-d H:m:s.S");
+			String[] simulationDetails = line.split(cvsSplitBy);
+
+			long blockId = Long.parseLong(simulationDetails[0]);
+			Date projDate = null;
+			try {
+				projDate = formatter.parse(simulationDetails[1]);
+			} catch(Exception e) 
+			{ 
+				e.printStackTrace(); 
+			}
+			
+			//Uncomment one of the block to run the corresponding algorithm
+			
+			/*Probabilistic probAlgo = new Probabilistic();
+			probAlgo.fetchRoute(blockId,projDate);
+			Probabilistic.total_time=0;*/
+			
+			/*Deterministic d = new Deterministic();
+			d.fetchRoute(blockId, projDate);*/
+			
+			/*Baseline b = new Baseline();
+			b.fetchRoute(blockId, projDate);*/
+		}
+
 	}
 }
