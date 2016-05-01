f7d747e8b94cf55a45151186285b2abcab3a96b0
c0deForLife <akolkarswapnil89@gmail.com>
2016-05-01 17:40:55 -0500

    Final changes to integrate UI with backend code

--- SpatioTemporalResourceSearch/src/com/src/spatiotemporal/servlet/AlgorithmSelection.java
+++ SpatioTemporalResourceSearch/src/com/src/spatiotemporal/servlet/AlgorithmSelection.java
@@ -2,7 +2,7 @@ package com.src.spatiotemporal.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -11,12 +11,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.src.spatiotemporal.Algorithms.Probabilistic;
 import com.src.spatiotemporal.Algorithms.Algorithms;
 import com.src.spatiotemporal.Algorithms.Baseline;
 import com.src.spatiotemporal.Algorithms.Deterministic;
 import com.src.spatiotemporal.Entity.Block;
 import com.src.spatiotemporal.Entity.Node;
-import com.src.spatiotemporal.Main.SpatioTemporal;
 
 public class AlgorithmSelection extends HttpServlet{
 	
@@ -24,12 +24,17 @@ public class AlgorithmSelection extends HttpServlet{
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
 	}
+	@SuppressWarnings("deprecation")
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 			String algoSelection = request.getParameter("algo");
 			String sec = request.getParameter("sec");
 			String min = request.getParameter("min");
 			String hour = request.getParameter("hour");
+			String dd = request.getParameter("date");
+			String mm = request.getParameter("mon");
+			String yy = request.getParameter("year");
+			
 			String originBlockId = request.getParameter("origin");
 			
 			Block block = new Block();
@@ -41,15 +46,17 @@ public class AlgorithmSelection extends HttpServlet{
 			else if(algoSelection.equals("D"))
 				chooseAlgo = new Deterministic();
 			else 
-				chooseAlgo = new Deterministic();
+				chooseAlgo = new Probabilistic();
 			
-			List<Node> nodeList = chooseAlgo.fetchRoute(block,hour, min, sec);
-			
-			/* CODE FOR TESTING NEED TO REMOVE IT LATER*/			
-			nodeList = new ArrayList<Node>();
-			for(int i=0; i<5;i++)
-				nodeList.add(SpatioTemporal.nodeList.get(i));
-			/* CODE FOR TESTING NEED TO REMOVE IT LATER*/
+			Date inputDate = new Date();
+			inputDate.setSeconds(Integer.parseInt(sec));
+			inputDate.setMinutes(Integer.parseInt(min));
+			inputDate.setHours(Integer.parseInt(hour));
+			inputDate.setDate(Integer.parseInt(dd));
+			inputDate.setMonth(Integer.parseInt(mm)-1);
+			inputDate.setYear(Integer.parseInt(yy)-1900);
+
+			List<Node> nodeList = chooseAlgo.fetchRoute(Long.parseLong(originBlockId), inputDate);			
 			
 			String json = new Gson().toJson(nodeList);
 			PrintWriter out = response.getWriter();
